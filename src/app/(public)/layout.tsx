12c54779d0c1c7459ea897037a1fc216c0e6c188
EponymousBearer <adnasir607@gmail.com>
2026-02-14 13:07:28 +0500

    inventory responsive products visibility header fixed

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -9,9 +9,9 @@ export default function PublicLayout({
 }) {
   return (
     <CartProvider>
-      {/* <Header /> */}
+      <Header />
       <main>{children}</main>
-      {/* <Footer /> */}
+      <Footer />
     </CartProvider>
   );
 }
